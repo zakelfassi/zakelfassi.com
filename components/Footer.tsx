25f89e1fb3d7de38ea9e71cfdf7cb0c1fd02e49f
zakelfassi <zakelfassi@gmail.com>
2024-01-28 10:20:23 -0800

    fix footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -16,15 +16,12 @@ export default function Footer() {
           <SocialIcon kind="facebook" href={siteMetadata.facebook} size={6} />
           <SocialIcon kind="youtube" href={siteMetadata.youtube} size={6} />
         </div>
-        <div className="mb-2 flex space-x-2 text-sm text-gray-500 dark:text-gray-400">
+        <div className="mb-8 flex space-x-2 text-sm text-gray-500 dark:text-gray-400">
           <div>{siteMetadata.author}</div>
           <div>{` • `}</div>
           <div>{`© ${new Date().getFullYear()}`}</div>
           <div>{` • `}</div>
           <Link href="/">{siteMetadata.title}</Link>
-        </div>
-        <div className="mb-8 text-sm text-gray-500 dark:text-gray-400">
-          <Link href="https://github.com/zakelfassi/zakelfassi.com">[Find on GitHub]</Link>
         </div>
       </div>
     </footer>
