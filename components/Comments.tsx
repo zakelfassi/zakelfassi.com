87719047616c71acda38390283bdd16459255aeb
zakelfassi <zakelfassi@gmail.com>
2024-01-28 21:06:42 -0800

    only show comments if something's available

--- components/Comments.tsx
+++ components/Comments.tsx
@@ -27,15 +27,17 @@ export default function Comments({
   return (
     <>
       {/* Share and Comment Section */}
-      <div className="flex flex-row items-center justify-center space-x-8">
-        <h2 className="text-2xl font-bold leading-8 tracking-tight">Share and Comment:</h2>
-        {ig && <SocialIcon kind="instagram" href={ig} size={6} />}
-        {ig && <SocialIcon kind="threads" href={threads} size={6} />}
-        {x && <SocialIcon kind="twitter" href={x} size={6} />}
-        {fb && <SocialIcon kind="facebook" href={fb} size={6} />}
-        {reddit && <SocialIcon kind="reddit" href={reddit} size={6} />}
-        {pinterest && <SocialIcon kind="pinterest" href={pinterest} size={6} />}
-      </div>
+      {(ig || threads || x || fb || reddit || pinterest) && (
+        <div className="flex flex-row items-center justify-center space-x-8">
+          <h2 className="text-2xl font-bold leading-8 tracking-tight">Share and Comment:</h2>
+          {ig && <SocialIcon kind="instagram" href={ig} size={6} />}
+          {threads && <SocialIcon kind="threads" href={threads} size={6} />}
+          {x && <SocialIcon kind="twitter" href={x} size={6} />}
+          {fb && <SocialIcon kind="facebook" href={fb} size={6} />}
+          {reddit && <SocialIcon kind="reddit" href={reddit} size={6} />}
+          {pinterest && <SocialIcon kind="pinterest" href={pinterest} size={6} />}
+        </div>
+      )}
     </>
   )
 }
