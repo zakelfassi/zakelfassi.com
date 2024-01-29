42e8d8ff23885bd251ad995e7a0eeb7466f156b8
zakelfassi <zakelfassi@gmail.com>
2024-01-28 17:30:00 -0800

    move newsletter to footer with social icons (to stay updated!)

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,12 +1,18 @@
 import Link from './Link'
 import siteMetadata from '@/data/siteMetadata'
 import SocialIcon from '@/components/social-icons'
+import NewsletterForm from 'pliny/ui/NewsletterForm'
 
 export default function Footer() {
   return (
     <footer>
       <div className="mt-16 flex flex-col items-center">
-        <div className="mb-3 flex space-x-4">
+        {siteMetadata.newsletter?.provider && (
+          <div className="flex items-center justify-center pt-4">
+            <NewsletterForm title="Stay in the loop" />
+          </div>
+        )}
+        <div className="mb-3 mt-8 flex space-x-4">
           <SocialIcon kind="mail" href={`mailto:${siteMetadata.email}`} size={6} />
           <SocialIcon kind="threads" href={siteMetadata.threads} size={6} />
           <SocialIcon kind="instagram" href={siteMetadata.instagram} size={6} />
