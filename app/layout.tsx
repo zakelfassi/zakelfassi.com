2f7123e3b541accb518781ecdd3767837fdef8fb
zakelfassi <zakelfassi@gmail.com>
2024-01-28 22:12:39 -0800

    update favicons

--- app/layout.tsx
+++ app/layout.tsx
@@ -66,9 +66,21 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       className={`${space_grotesk.variable} scroll-smooth`}
       suppressHydrationWarning
     >
-      <link rel="apple-touch-icon" sizes="76x76" href="/static/favicons/apple-touch-icon.png" />
-      <link rel="icon" type="image/png" sizes="32x32" href="/static/favicons/favicon-32x32.png" />
-      <link rel="icon" type="image/png" sizes="16x16" href="/static/favicons/favicon-16x16.png" />
+      <link rel="apple-touch-icon" sizes="152x152" href="/static/favicons/apple-touch-icon.png" />
+      <link
+        rel="icon"
+        type="image/png"
+        href="/static/favicons/favicon-196x196.png"
+        sizes="196x196"
+      />
+      <link rel="icon" type="image/png" href="/static/favicons/favicon-128.png" sizes="128x128" />
+      <meta name="application-name" content="&nbsp;" />
+      <meta name="msapplication-TileColor" content="#FFFFFF" />
+      <meta name="msapplication-TileImage" content="/static/favicons/mstile-144x144.png" />
+      <meta name="msapplication-square70x70logo" content="/static/favicons/mstile-70x70.png" />
+      <meta name="msapplication-square150x150logo" content="/static/favicons/mstile-150x150.png" />
+      <meta name="msapplication-wide310x150logo" content="/static/favicons/mstile-310x150.png" />
+      <meta name="msapplication-square310x310logo" content="/static/favicons/mstile-310x310.png" />
       <link rel="manifest" href="/static/favicons/site.webmanifest" />
       <link rel="mask-icon" href="/static/favicons/safari-pinned-tab.svg" color="#5bbad5" />
       <meta name="msapplication-TileColor" content="#000000" />
